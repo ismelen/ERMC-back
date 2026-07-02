b619e37650fbf05d5977f4c372456fa5085ccea9
ismelen <ismael.ma34k@gmail.com>
2026-07-02 20:27:53 +0200

    feat(front): eas

--- front/mobile/app.config.js
+++ front/mobile/app.config.js
@@ -4,6 +4,9 @@ export default ({ config }) => ({
   ...config,
   extra: {
     backendUrl: process.env.BACKEND_URL,
-    dropboxApiKey: process.env.DROPBOX_API_KEY
+    dropboxApiKey: process.env.DROPBOX_API_KEY,
+    eas: {
+      projectId: "2ab9e73c-2017-467b-87c4-d969765dd9c0"
+    }
   },
 });
